b21f77f591c8f4a5eb30f24cc58795f5643a361d
KhelemeliaOleksii <xvdm@ukr.net>
2022-05-06 20:06:46 +0300

    add statictic general parameters

--- src/components/FeedBack/FeedBack.js
+++ src/components/FeedBack/FeedBack.js
@@ -9,24 +9,43 @@ class FeedBack extends Component {
             bad: 0
         }
     }
-
+    handlerOnClick = (key) => {
+        this.setState((prevState) => { return { [key]: prevState[key] + 1 } });
+        // console.log(this.state[key]);
+    }
+    countTotalFeedback() {
+        return Object.values(this.state).reduce((total, item) => total + item, 0);
+    }
     render() {
         const entries = Object.entries(this.state);
         return (
             <div className="FeedBack">
-                {entries.map(([key]) => {
-                    return (
+                <h2 className="Title">Please leave feedback</h2>
+                <div className="Controls">
+                    {entries.map(([key]) => (
                         <button
                             key={key}
                             type="button"
                             className="button"
                             name={key}
+                            onClick={() => this.handlerOnClick(key)}
                         >
                             {key}
                         </button>
-                    )
-                }
-                )}
+                    ))}
+                </div>
+                <div className="Statistics">
+                    <h3 className="Statistic--title">Statistics</h3>
+                    {entries.map(([key, value]) => (
+                        <p
+                            key={key}
+                            className="label"
+                        >
+                            <span>{key} : </span>
+                            <span>{value}</span>
+                        </p>
+                    ))}
+                </div>
             </div>
         )
     }
