88ef2086fcd3a308743081fa8cf2981b1b0bf7ea
KhelemeliaOleksii <xvdm@ukr.net>
2022-05-21 11:52:12 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,6 +28,7 @@ export const App = () => {
   const countTotalFeedback = () => {
     return good + bad + neutral;
   }
+
   const countPositiveFeedbackPercentage = () => {
     const total = countTotalFeedback();
     if (total <= 0) {
@@ -55,53 +56,4 @@ export const App = () => {
       </Section>
     </>
   )
-
 }
-
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   }
-//   handlerOnClick = (key) => {
-//     this.setState((prevState) => ({ [key]: prevState[key] + 1 }));
-
-//   }
-//   countTotalFeedback() {
-//     return Object.values(this.state).reduce((total, item) => total + item, 0);
-//   }
-//   countPositiveFeedbackPercentage() {
-//     const total = this.countTotalFeedback();
-//     if (total <= 0) {
-//       return 0;
-//     }
-//     return this.state.good / total * 100;
-//   }
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     const total = this.countTotalFeedback();
-//     const keysState = Object.keys(this.state);
-
-//     return (
-//       <>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions keys={keysState} onLeaveFeedback={this.handlerOnClick} />
-//         </Section>
-//         <Section title="Statistics">
-//           {total ?
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={this.countTotalFeedback()}
-//               positivePercentage={this.countPositiveFeedbackPercentage()} />
-//             :
-//             <Notification message="There is no feedback" />
-//           }
-//         </Section>
-//       </>
-//     )
-//   }
-// }
